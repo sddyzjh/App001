a06ddc305f96387487de3c2d0734c6d406ccead4
sddyzjh <463722952@qq.com>
2016-10-07 22:10:43 +0800

    Create Constants.java

---
+++ app/src/main/java/com/example/saint/app001/Constants.java
@@ -0,0 +1,24 @@
+package com.example.saint.app001;
+
+/**
+ * Created by Saint on 2016/10/5.
+ */
+
+public class Constants {
+    //广播动作-播放
+    public static final String PLAY = "com.example.action.PLAY";
+    //广播动作-暂停
+    public static final String PAUSE = "com.saint.action.PAUSE";
+    //广播动作-上一首
+    public static final String LAST = "com.saint.action.LAST";
+    //广播动作-下一首
+    public static final String NEXT = "com.saint.action.NEXT";
+    //广播动作-循环播放
+    public static final String ROUND_PLAY = "com.saint.action.ROUND_PLAY";
+    //广播动作-随机播放
+    public static final String RANDOM_PLAY = "com.saint.action.RANDOM_PLAY";
+    //广播动作-刷新布局
+    public static final String REFRESH_LAYOUT = "com.saint.action.REFRESH_LAYOUT";
+    //广播动作-暂停与播放切换
+    public static final String CHANGE = "com.saint.action.CHANGE_STATUS";
+}
